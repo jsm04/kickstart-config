f3213c55a02ca320ee2f61154ea2b1610a21c0b3
jsm <jsmoretti.dev@outlook.com>
2025-03-26 21:25:22 -0300

    updates

--- init.lua
+++ init.lua
@@ -213,6 +213,8 @@ vim.keymap.set('n', '<C-M-k>', ':resize +2<CR>', { noremap = true, silent = true
 vim.keymap.set('n', '<Leader>bd', ':bd<CR>', { noremap = true, silent = true })
 -- Close buffer without closing the window
 vim.keymap.set('n', '<Leader>bD', ':bp | bd #<CR>', { noremap = true, silent = true })
+-- Go to mark
+vim.api.nvim_set_keymap('n', 'gm', '`', { noremap = true, silent = true })
 
 -- Diagnostics disable text toggle
 vim.keymap.set('n', '<leader>td', function()
@@ -252,6 +254,8 @@ vim.cmd [[
 vim.g.netrw_sort_by = 'name' -- Sort files by name (can change to 'time' or 'size')
 vim.g.netrw_show_hidden = 1 -- Show hidden files (e.g., .git, .vimrc)
 vim.g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
+vim.g.netrw_liststyle = 0 -- Enable tree view
+vim.g.netrw_banner = 1 -- Disable banner
 
 -- Open netrw explorer
 vim.api.nvim_set_keymap('n', '<leader>e', ':Explore<CR>', { noremap = true, silent = true })
@@ -549,31 +553,6 @@ require('lazy').setup({
       'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
-      -- Brief aside: **What is LSP?**
-      --
-      -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
-      -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-      -- and language tooling communicate in a standardized fashion.
-      --
-      -- In general, you have a "server" which is some tool built to understand a particular
-      -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-      -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-      -- processes that communicate with some "client" - in this case, Neovim!
-      --
-      -- LSP provides Neovim with features like:
-      --  - Go to definition
-      --  - Find references
-      --  - Autocompletion
-      --  - Symbol Search
-      --  - and more!
-      --
-      -- Thus, Language Servers are external tools that must be installed separately from
-      -- Neovim. This is where `mason` and related plugins come into play.
-      --
-      -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-      -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
       --  This function gets run when an LSP attaches to a particular buffer.
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
